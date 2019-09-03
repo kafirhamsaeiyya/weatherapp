2deb5964845ae89726091c2992e717026d738c33
89989 <rohandeshpandi@gmail.com>
2019-09-03 00:06:53 -0400

    added https to API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function query() {
   var apiKey = '92cdcdc2ac5c211e6900789a350685d6';
   var city = document.getElementById("weatherinput").innerHTML;
-  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   console.log(city);
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
